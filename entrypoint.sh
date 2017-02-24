c9ee20218db58cd5ee892eea8d76487c1408a6c6
Sheogorath <sheogorath@shivering-isles.com>
2017-02-24 02:14:44 +0100

    Fix config generation

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,10 @@ NGX_UPSTREAM=${NGX_ADDRESS:-app}:${NGX_PORT:-80}
 sed -i 's/NGX_UPSTREAM/'"${NGX_UPSTREAM}"'/' /etc/nginx/upstream.conf
 
 # Generate Pagespeed config based on environment variables
-echo $(env | grep NPSC_ | sed -e 's/^NPSC_//g' -e "s/\([A-Z_]*\)=/\L\1=/g" -e "s/_\([a-zA-Z]\)/\u\1/g" -e "s/^\([a-zA-Z]\)/\u\1/g" -e 's/\=/ /' -e 's/^/\pagespeed /' -e 's/$/;/') >> /etc/nginx/pagespeed.conf
+env | grep NPSC_ | sed -e 's/^NPSC_//g' -e "s/\([A-Z_]*\)=/\L\1=/g" -e "s/_\([a-zA-Z]\)/\u\1/g" -e "s/^\([a-zA-Z]\)/\u\1/g" -e 's/=.*$//' -e 's/\=/ /' -e 's/^/\pagespeed /' > /tmp/pagespeed-prefix.txt
+
+env | grep NPSC_ | sed -e 's/^[^=]*=//' -e 's/$/;/' > /tmp/pagespeed-suffix.txt
+
+paste -d" " /tmp/pagespeed-prefix.txt /tmp/pagespeed-suffix.txt >> /etc/nginx/pagespeed.conf
 
 exec "$@"
