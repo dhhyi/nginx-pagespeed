f0c979bb719ba92a8043b29c624af1911906814f
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-09-10 08:45:38 +0200

    added caching for content

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,13 @@ set -e
 
 NGX_UPSTREAM=${NGX_ADDRESS:-app}:${NGX_PORT:-80}
 
+NGX_CACHE_DURATION="${NGX_CACHE_DURATION:-"10m"}"
+
 [[ $DEBUG == true ]] && set -x
 
 # Generate Upstream config
-sed -i 's/NGX_UPSTREAM/'"${NGX_UPSTREAM}"'/' /etc/nginx/upstream.conf
+sed -i 's%NGX_UPSTREAM%'"${NGX_UPSTREAM}"'%' /etc/nginx/upstream.conf
+sed -i 's%NGX_CACHE_DURATION%'"${NGX_CACHE_DURATION}"'%' /etc/nginx/upstream.conf
 
 # Generate Pagespeed config based on environment variables
 env | grep NPSC_ | sed -e 's/^NPSC_//g' -e "s/\([A-Z_]*\)=/\L\1=/g" -e "s/_\([a-zA-Z]\)/\u\1/g" -e "s/^\([a-zA-Z]\)/\u\1/g" -e 's/=.*$//' -e 's/\=/ /' -e 's/^/\pagespeed /' > /tmp/pagespeed-prefix.txt
